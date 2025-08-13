2e25244e63290c23b44052dcc2f915eb31035340
Abdel <abdulrashidissah232@gmail.com>
2025-08-13 14:30:30 +0000

    feat: add tags property to CreateItemDto for better item categorization

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,8 +1,10 @@
 import { CreateListingDto } from './create-listing.dto';
+import { CreateTag } from './create-tag.dto';
 
 export class CreateItemDto {
   name: string;
   public?: boolean;
   listing: CreateListingDto;
   comments: string[];
+  tags: CreateTag[];
 }
