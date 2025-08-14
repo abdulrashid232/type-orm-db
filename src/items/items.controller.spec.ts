629bb5ec419c3e65e05dfd5a261e88b168b55076
Abdel <abdulrashidissah232@gmail.com>
2025-08-14 11:54:40 +0000

    test: enhance ItemsController tests with mock repository and entity manager

--- src/items/items.controller.spec.ts
+++ src/items/items.controller.spec.ts
@@ -1,14 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
+import { EntityManager } from 'typeorm';
 
 describe('ItemsController', () => {
   let controller: ItemsController;
 
   beforeEach(async () => {
+    const mockItemRepository = {};
+    const mockEntityManager = {};
+
     const module: TestingModule = await Test.createTestingModule({
+      imports: [],
       controllers: [ItemsController],
-      providers: [ItemsService],
+      providers: [
+        {
+          provide: getRepositoryToken(Item),
+          useValue: mockItemRepository,
+        },
+        {
+          provide: EntityManager,
+          useValue: mockEntityManager,
+        },
+        ItemsService,
+      ],
     }).compile();
 
     controller = module.get<ItemsController>(ItemsController);
