4394b3eb2763d67afe094974bbeefb6abccf3383
Abdel <abdulrashidissah232@gmail.com>
2025-08-14 12:22:20 +0000

    refactor: enhance response structure in ItemsController methods

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
 } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { CreateItemDto } from './dto/create-item.dto';
@@ -17,26 +18,48 @@ export class ItemsController {
 
   @Post()
   async create(@Body() createItemDto: CreateItemDto) {
-    return await this.itemsService.create(createItemDto);
+    const item = await this.itemsService.create(createItemDto);
+    return {
+      message: 'Item created successfully',
+      status: 'success',
+      data: item,
+    };
   }
 
   @Get()
   async findAll() {
-    return await this.itemsService.findAll();
+    const items = await this.itemsService.findAll();
+    return {
+      message: 'Items retrieved successfully',
+      status: 'success',
+      data: items,
+    };
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    return await this.itemsService.findOne(+id);
+    const item = await this.itemsService.findOne(+id);
+    return {
+      message: 'Item retrieved successfully',
+      status: 'success',
+      data: item,
+    };
   }
 
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateItemDto: UpdateItemDto) {
-    return await this.itemsService.update(+id, updateItemDto);
+    const updatedItem = await this.itemsService.update(+id, updateItemDto);
+    return {
+      message: 'Item updated successfully',
+      status: 'success',
+      data: updatedItem,
+    };
   }
 
   @Delete(':id')
+  @HttpCode(204)
   async remove(@Param('id') id: string) {
-    return await this.itemsService.remove(+id);
+    await this.itemsService.remove(+id);
+    return { message: 'Item removed successfully', status: 'success' };
   }
 }
