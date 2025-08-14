5b2f1aaa6dc6be928c3e016060c906aad7c58f36
Abdel <abdulrashidissah232@gmail.com>
2025-08-14 11:54:23 +0000

    test: enhance ItemsService tests with mock repository and entity manager

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ItemsService } from './items.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
+import { EntityManager } from 'typeorm';
 
 describe('ItemsService', () => {
   let service: ItemsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ItemsService],
+      providers: [
+        ItemsService,
+        {
+          provide: getRepositoryToken(Item),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            save: jest.fn(),
+            findOneBy: jest.fn(),
+          },
+        },
+        {
+          provide: EntityManager,
+          useValue: {
+            save: jest.fn(),
+            transaction: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ItemsService>(ItemsService);
