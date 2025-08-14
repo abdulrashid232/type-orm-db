01574ff7186b48c8dd7300e1234379a136fb5635
Abdel <abdulrashidissah232@gmail.com>
2025-08-14 12:02:46 +0000

    test: add unit tests for item creation and retrieval in ItemsService

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -3,6 +3,8 @@ import { ItemsService } from './items.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Item } from './entities/item.entity';
 import { EntityManager } from 'typeorm';
+import { CreateItemDto } from './dto/create-item.dto';
+import { CreateListingDto } from './dto/create-listing.dto';
 
 describe('ItemsService', () => {
   let service: ItemsService;
@@ -35,5 +37,66 @@ describe('ItemsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create an item', async () => {
+    const listing = new CreateListingDto();
+    listing.rating = 5;
+    listing.description = 'Test Description';
+    const itemData: CreateItemDto = {
+      name: 'Test Item',
+      listing: listing,
+      comments: [],
+      tags: [],
+    };
+
+    jest.spyOn(service, 'create').mockResolvedValue({
+      id: 1,
+      name: 'Test Item',
+      listing: {
+        id: 1,
+        description: 'Test Description',
+        rating: 5,
+      },
+      comments: [],
+      tags: [],
+      public: true,
+    });
+
+    const result = await service.create(itemData);
+    expect(result).toEqual({
+      id: 1,
+      name: 'Test Item',
+      listing: {
+        id: 1,
+        description: 'Test Description',
+        rating: 5,
+      },
+      comments: [],
+      tags: [],
+      public: true,
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of items', async () => {
+      const result = [
+        {
+          id: 1,
+          name: 'Test Item',
+          listing: {
+            id: 1,
+            description: 'Test Description',
+            rating: 5,
+          },
+          comments: [],
+          tags: [],
+          public: true,
+        },
+      ];
+      jest.spyOn(service, 'findAll').mockResolvedValue(result);
+
+      expect(await service.findAll()).toEqual(result);
+    });
   });
 });
